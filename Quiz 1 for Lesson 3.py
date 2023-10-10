7aca19a81be0f415bb7d71e3ea6bb3bfe22d6acc
Anna Klymchuk <138696149+AnyKlymchuk@users.noreply.github.com>
2023-10-10 11:42:59 +0300

    Add files via upload

---
+++ Quiz 1 for Lesson 3.py
@@ -0,0 +1,11 @@
+"""value_one = 5 ** 2
+value_two = 5 ** 3
+
+print(value_one)
+print(value_two)"""
+
+variable_1 = 1
+variable_2 = 2
+variable_3 = "3"
+
+print(variable_1 + variable_2 + variable_3)
